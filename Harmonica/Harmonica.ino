0c981e25454df237e0887b526f80e7b43f17e2d4
shlonkin <shlonkin@users.noreply.github.com>
2016-10-01 19:54:04 +0900

    Update Harmonica.ino

--- Harmonica/Harmonica.ino
+++ Harmonica/Harmonica.ino
@@ -9,6 +9,17 @@
  * 
  * This code does:
  * - Normally it is listening to mic input and determining note frequency
+/*
+ * Classroom music teaching aid
+ * Teensy3.2 code
+ * by shlonkin
+ * 
+ * This makes use of Teensyduino libraries, in particular the Audio libraries.
+ * See the corresponging files for their licence info.
+ * This code is in the public domain.
+ * 
+ * This code does:
+ * - Normally it is listening to mic input and determining note frequency
  *   via the AudioAnalyzeNoteFrequency library.
  * - When a note is detected, the note number(1-15) is sent by Serial to
  *   the ATMega to turn on the LEDs. When the note is no longer playing, 
@@ -33,6 +44,8 @@
 #define SDCARD_CS_PIN    10
 #define SDCARD_MOSI_PIN  7
 #define SDCARD_SCK_PIN   14
+#define PLAYPIN 2
+#define RECPIN 3
 
 // a mapping of note numbers to sample filenames
 const char noteFiles[15][7] = {"G4.WAV", "A5.WAV", "B5.WAV", "C5.WAV", "D5.WAV"
@@ -43,7 +56,6 @@ const int myInput = AUDIO_INPUT_MIC;
 
 // The Audio components
 AudioInputI2S audioInput;
-AudioPlayMemory samplePlayer;
 AudioAnalyzeNoteFrequency noteFreq;
 AudioOutputI2S audioOutput;
 AudioPlaySdWav noteSample;
@@ -51,13 +63,12 @@ AudioPlaySdWav noteSample;
 // Connect the input
 AudioConnection c1(audioInput, 0, noteFreq, 0);
 // and the output
-AudioConnection c2(samplePlayer, 0, audioOutput, 0);
+AudioConnection c2(noteSample, 0, audioOutput, 0);
 
 AudioControlSGTL5000 audioShield;
 
-// Bounce objects to read buttons (pins 2 and 3)
-Bounce playButton = Bounce(2, 15);
-Bounce recordButton = Bounce(3, 15);
+// the threshold parameter for note frequency
+float nfThreshold;
 
 void setup() {
   
@@ -66,19 +77,34 @@ void setup() {
   // Enable the audio shield and set the output volume.
   audioShield.enable();
   audioShield.inputSelect(AUDIO_INPUT_MIC);
-  audioShield.volume(0.5);
+  audioShield.volume(0.9);
 
   // init noteFreq
-  noteFreq.begin(.15);
+  // read the volume pot on the audio shield
+  // the yin parameter is 0.05 + val/2048
+  // this gives a value between 0.05 and 0.55
+  nfThreshold = analogRead(15);
+  nfThreshold = 0.05 + nfThreshold/2048;
+  noteFreq.begin(nfThreshold);
+  noteFreq.minFrequency(370);
 
   // use Serial1
   Serial1.begin(9600);
+  delay(100);
   // Wait till the ATMega chip is ready.
   // It will send a character, respond with one.
-  while(Serial1.available() < 1);
-  Serial1.write('A');
+  byte ackReceived = 0;
+  while(ackReceived < 1){
+    while(Serial1.available() < 1);
+    if(Serial1.read() == 0xAC){
+      ackReceived = 1;
+    }
+  }
+  Serial1.write(0xAC);
+  // may have to delay up to 100ms
+  delay(100);
   Serial1.clear();
-
+  
   // set up the SD card
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
@@ -91,6 +117,11 @@ void setup() {
       delay(1000);
     }
   }
+  
+  pinMode(PLAYPIN, INPUT_PULLUP);
+  pinMode(RECPIN, INPUT_PULLUP);
+  
+  lightSequence();
 }
 
 /*
@@ -100,6 +131,7 @@ void setup() {
  * mic mode. The two can't be done simultaneously.
  */
 void loop() {
+  
   if(Serial1.available() > 0){
     keyboardMode();
   }
@@ -107,7 +139,77 @@ void loop() {
   if (noteFreq.available()) {
     micMode();
   }
+  
+  if(digitalRead(PLAYPIN) == LOW){
+    if(digitalRead(RECPIN) == LOW){
+      // if both buttons are pressed, go into parameter changing mode
+      parameterChange();
+    }
     
+  }else if(digitalRead(RECPIN) == LOW){
+    testLoop();
+  }
+  
+}
+
+void testLoop(){
+  // plays one note
+  Serial1.write(3);
+  startSample(3);
+  delay(200);
+  stopSample(3);
+  Serial1.write(0xFF);
+  
+  /*
+  // plays each note
+  byte n = 0;
+  while(n < 15){
+    Serial1.write(n);
+    startSample(n);
+    delay(150);
+    stopSample(n);
+    Serial1.write(0xFF);
+    n++;
+  }
+  */
+}
+
+void parameterChange(){
+  float param = nfThreshold;
+  byte light = nfThreshold * 28 - 1;
+  byte newLight = light;
+  Serial1.write(light);
+  // stay here for 5 seconds
+  unsigned long startTime = millis();
+  while(millis() - startTime < 5000){
+    param = analogRead(15);
+    param = 0.05 + param/2048;
+    newLight = param * 28 - 1;
+    if(newLight != light){
+      Serial1.write(0xFF);
+      Serial1.write(newLight);
+      light = newLight;
+    }
+  }
+  if(param < 0){
+    param = 0.01;
+  }
+  if(param > 0.9){
+    param = 0.9;
+  }
+  nfThreshold = param;
+  noteFreq.threshold(nfThreshold);
+  Serial1.write(0xFF);
+}
+
+void lightSequence(){
+byte n = 0;
+  while(n < 15){
+    Serial1.write(n);
+    delay(70);
+    Serial1.write(0xFF);
+    n++;
+  }
 }
 
 void keyboardMode(){
@@ -125,7 +227,7 @@ void keyboardMode(){
     if(Serial1.available() > 0){
       in = Serial1.read();
       if(in == 0xFF){
-        stopSample();
+        stopSample(in);
         stillPlaying = false;
       }else if(in <14){
         // this should only happen if the note changed
@@ -144,41 +246,39 @@ void micMode(){
     return;
   }
   Serial1.write(note);
-  // do some simple filtering
-  byte burps = 0;
-  while(burps < 5){
-    burps = 0;
-    for(byte i=0; i<10; i++){
-      if(note != fToNote(noteFreq.read())){
-        burps++;
-      }
-      delay(10);
-    }
-    noteFreq.available(); //Just to clear the flag
+  // Wait till the read data is wrong.
+  // The data will update at an unpredictable rate.
+  // Just keep checking till it changes.
+  while(fToNote(noteFreq.read()) == note){
+    delay(10);
   }
+  
   Serial1.write(0xFF);
+  noteFreq.available(); //Just to clear the flag
 }
 
 void startSample(byte n){
-  // increase the volume over 250ms
-  audioShield.volume(0.01);
+  // increase the volume over 30ms
+  audioShield.volume(0.03);
   noteSample.play(noteFiles[n]);
-  for(int i=1; i<=25; i++){
-    audioShield.volume(0.02*i);
-    delay(10);
+  for(int i=1; i<=30; i++){
+    audioShield.volume(0.03*i);
+    delay(1);
   }
 }
 
-void stopSample(){
-  // decrease the volume over 250ms
-  for(int i=25; i>0; i--){
+void stopSample(byte n){
+  // decrease the volume over 30ms
+  for(int i=30; i>0; i--){
     if(noteSample.isPlaying()){
-      audioShield.volume(0.02*i);
-      delay(10);
+      audioShield.volume(0.03*i);
+      delay(1);
+    }else{
+      noteSample.play(noteFiles[n]);
     }
   }
   noteSample.stop();
-  audioShield.volume(0.5);
+  audioShield.volume(0.03);
 }
 
 byte fToNote(float f){
